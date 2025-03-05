41944a529c10ab289e7cc04c1cf012ad04b9a1b8
Cadecc <fgh456hgf@gmail.com>
2025-03-06 01:35:45 +0800

    3/6 更新collapse字體大小

---
+++ html-sidebar.php
@@ -0,0 +1,44 @@
+<aside class="sidebar">
+    <h4 class="sidebar-brand">海優斯後台管理</h4>
+    <ul class="nav flex-column sidebar-nav ">
+        <li class="nav-item d-flex align-items-center fs-5">
+            <i class="bi bi-house "></i>
+            <a href="index_.php" class="nav-link fw-bold" aria-current="page">首頁</a>
+        </li>
+        <li class="nav-item d-flex align-items-center fs-5 active">
+            <i class="bi bi-person-circle"></i>
+            <a href="accountList.php" class="nav-link fw-bold">
+                會員管理
+            </a>
+        </li>
+        <li class="nav-item ">
+            <div class="d-flex align-items-center fs-5">
+                <i class="bi bi-handbag"></i>
+                <a class="nav-link fw-bold" data-bs-toggle="collapse" href="#couponMenu" role="button" aria-expanded="false">
+                    優惠卷管理
+                </a>
+            </div>
+            <div class="collapse" id="couponMenu">
+                <ul class="list-group ">
+                    <a class="list-group-item bg-light fs-6" href="#">優惠卷內容管理</a>
+                    <a class="list-group-item bg-light fs-6" href="#">會員優惠卷管理</a>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item d-flex align-items-center fs-5">
+            <i class="bi bi-shop"></i>
+            <a href="#" class="nav-link fw-bold">商品管理</a>
+        </li>
+        <li class="nav-item d-flex align-items-center fs-5">
+            <i class="bi bi-person-standing"></i>
+            <a href="#" class="nav-link fw-bold">教練管理</a>
+        </li>
+        <li class="nav-item d-flex align-items-center fs-5">
+            <i class="bi bi-file-text"></i>
+            <a href="#" class="nav-link fw-bold">文章管理</a>
+        </li>
+        <li class="nav-item d-flex justify-content-end fs-5">
+            <a href="login.php" class="nav-link fw-bold">登入</a>
+        </li>
+    </ul>
+</aside>
